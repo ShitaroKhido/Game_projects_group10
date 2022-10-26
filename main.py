f08662851ab23df7433413d7ee215a7f7abfc79f
ShitaroKhido <khidpagna01@gmail.com>
2022-10-26 09:02:30 +0700

    Update

--- main.py
+++ main.py
@@ -1,67 +1,18 @@
-from tkinter import *
+from tkinter import Tk, Canvas, mainloop, BOTH
 
-class Player:
+class Character:
     
-    PLAYER_POSITION = [10,10,120,120]
+    pass
 
-    def __init__(self, master, player, crosshair = None) -> None:
-        Canvas().__init__(master)
-        self._player_img = player
-        self._crosshair_img = crosshair
-        self._canvas = Canvas(master)
-        self._canvas.pack(expand=True, fill=BOTH)
-        self._player = self._canvas.create_image(10, 10 , image=self._player_img)
-        self._crosshair = self._canvas.create_image(
-            self._canvas.coords(self._player)[0]+20,
-            self._canvas.coords(self._player)[1]+20,
-            image=self._crosshair_img
-        )
-        self._master = master
-        ### MOVEMENTS KEY
-        self._master.bind("<w>", self.up)
-        self._master.bind("<a>", self.left)
-        self._master.bind("<s>", self.down)
-        self._master.bind("<d>", self.right)
-        ### AIMING MOTION
-        self._master.bind("<Motion>", self._player_crosshair)
 
-    def _player_crosshair(self, event):
-        self._canvas.delete(self._crosshair)
-        self._crosshair = self._canvas.create_image(
-            event.x,
-            event.y,
-            image=self._crosshair_img
-        )
-        print(f"{event.x} {event.y}")
-
-    def movements(self, x,y):
-        self._canvas.move(self._player, x, y)
-
+class Draw:
     
-    def right(self, event):
-        self.movements(20, 0)
+    def __init__(self, canvas, grid, x, y, grid_size):
+        self.canvas = canvas
+        self.grid = grid
+        self.grid_size = grid_size
+        self.x = x
+        self.y = y
 
-    def left(self, event):
-        self.movements(-20, 0)
-
-    def up(self, event):
-        self.movements(0, -20)
-
-    def down(self, event):
-        self.movements(0, 20)
-
-########################################################################################
-
-
-if __name__=="__main__":
-
-    root = Tk()
-    root.geometry("1000x600")
-    root.resizable(0,0)
-    
-    p1 = PhotoImage(file="Game_projects_group10/assets/image/virus2.png")
-    cross = PhotoImage(file="Game_projects_group10/assets/image/crosshairs.png")
-
-    player = Player(root,p1,cross)
-
-    mainloop()
+    def level(self):
+        pass
