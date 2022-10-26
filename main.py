4aafa73f349ee5338ead11b4ee7351a3ba84e3ea
animeoncean <ahseangeng777@gmail.com>
2022-10-26 14:04:39 +0700

    update

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ GAME_ICON_LOCATION = ""
 CHARACTER_IMG_LOCATION = "Game_projects_group10\\assets\image\\virus2.png"
 VIRUS_IMG_LOCATION = "Game_projects_group10\\assets\image\\b2.png"
 
+BACKGROUND_LEVEL_ONE="Game_projects_group10\\assets\image\LEVEL1.png"
+BACKGROUND_LEVEL_TWO="Game_projects_group10\\assets\image\LEVEL2.png"
+BACKGROUND_LEVEL_THREE="Game_projects_group10\\assets\image\LEVEL3.png"
 #################
 ### MAIN CODE ###
 #################
