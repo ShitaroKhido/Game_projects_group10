024d470a1f3b5f81492586881ec5e107db2a45ac
ShitaroKhido <khidpagna01@gmail.com>
2022-10-26 15:16:53 +0700

    Update

--- main.py
+++ main.py
@@ -11,34 +11,6 @@ CHARACTER_IMG_LOCATION = "Game_projects_group10\\assets\image\\virus2.png"
 VIRUS_IMG_LOCATION = "Game_projects_group10\\assets\image\\b2.png"
 CROSSHAIR = "Game_projects_group10\\assets\image\crosshairs.png"
 
-class Player:
-    
-    def __init__(self, master, canvas, position, player_img, player_crosshair) -> None:
-        self._canvas = canvas
-        self._player = self._canvas.create_image(position, image = player_img, tag="player")
-        self._player_crosshair = self._canvas.create_image(position, image = player_crosshair)
-        self._master = master
-
-        self._master.bind("<w>", self.move_up)
-        self._master.bind("<a>", self.move_left)
-        self._master.bind("<s>", self.move_down)
-        self._master.bind("<d>", self.move_right)
-        self._master.bind("<Motion>", self.player_crosshair)
-
-
-    def player_crosshair(self, event):
-        self._canvas.moveto(self._player_crosshair, event.x-40, event.y-40)
-    
-
-
-class Home:
-    
-
-    def __init__(self,master, canvas, background_location) -> None:
-        self.background = PhotoImage(file= background_location)
-        self.add_background = canvas.create_image(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, image=self.background)
-
-
 
 #################
 ### MAIN CODE ###
@@ -50,12 +22,6 @@ if __name__ == "__main__":
     root.title(GAME_TITLE)
     root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
 
-    main_canvas = Canvas(root)
-    main_canvas.pack(expand=True, fill=BOTH)
 
-    player_img = PhotoImage(file=CHARACTER_IMG_LOCATION)
-    player_crosshair = PhotoImage(file=CROSSHAIR)
-    
-    player  = Player(root, main_canvas, [100,100], player_img, player_crosshair )
 
     mainloop()
