2a184e4d333288935755d029082f94d2f398d379
ShitaroKhido <khidpagna01@gmail.com>
2022-10-26 14:01:17 +0700

    upt

--- main.py
+++ main.py
@@ -46,12 +46,13 @@ if __name__ == "__main__":
     root.title(GAME_TITLE)
     root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
 
+    ### MAIN GAME FRAME
     frame = Frame(root)
     frame.pack(expand=True, fill=BOTH)
-
+    ### MAIN CANVAS
     canvas = Canvas(frame)
     canvas.pack(expand=True, fill=BOTH)
-
+    ### PLAYER IMAGES
     player_img = PhotoImage(file=CHARACTER_IMG_LOCATION)
 
     main_player = Player(frame, canvas, [100,100], player_img)
