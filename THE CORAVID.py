df31e4b4fafac27bd6a83dab1c13b2fce2ac2139
ShitaroKhido <khidpagna01@gmail.com>
2022-10-27 14:06:55 +0700

    Update

--- THE CORAVID.py
+++ THE CORAVID.py
@@ -1,48 +1,7 @@
 from tkinter import Button, Frame, PhotoImage, Tk, Canvas, mainloop, BOTH
 from Game_projects_group10.library.mechanism import *
+from Game_projects_group10.library.constant import * 
 import winsound 
-
- 
-################
-### CONSTANT ###
-################
-
-
-WINDOW_WIDTH = 1000
-WINDOW_HEIGHT = 600
-GAME_TITLE = "THE CORAVID"
-
-GAME_ICON_LOCATION = ""
-CHARACTER_IMG_LOCATION = ""
-VIRUS_IMG_LOCATION = "Game_projects_group10\\assets\image\\b2.png"
-CROSSHAIR = "Game_projects_group10\\assets\image\crosshairs.png"
-ENEMY_IMG_LOCATION = "Game_projects_group10\\assets\image\\virus2.png"
-
-
-
-BLACK_IMG_LOCATION="Game_projects_group10\\assets\image\Black_background.png"
-BUTTON_START_IMG_LOCATION='Game_projects_group10\\assets\image\\button_start.png'
-BUTTON_SETTING_IMG_LOCATION='Game_projects_group10\\assets\image\\button_setting.png'
-BUTTON_EXIT_IMG_LOCATION='Game_projects_group10\\assets\image\\button_exit.png'
-BUTTON_BACK_IMG_LOCATION='Game_projects_group10\\assets\image\\button_back.png'
-
-
-BUTTON_LEVEL1_IMG_LOCATION='Game_projects_group10\\assets\image\\1.png'
-BUTTON_LEVEL2_IMG_LOCATION='Game_projects_group10\\assets\image\\2.png'
-BUTTON_LEVEL3_IMG_LOCATION='Game_projects_group10\\assets\image\\3.png'
-
-
-BUTTON_ON_IMG_LOCATION='Game_projects_group10\\assets\image\\no.png'
-BUTTON_OFF_IMG_LOCATION='Game_projects_group10\\assets\image\off.png'
-
-HOME_BACKGROUND_IMAGE_LOCATION="Game_projects_group10\\assets\image\\background.png"
-BACKGROUND_LEVEL1_LOCATION="Game_projects_group10\\assets\image\LEVEL1.png"
-BACKGROUND_LEVEL2_LOCATION="Game_projects_group10\\assets\image\LEVEL2.png"
-BACKGROUND_LEVEL3_LOCATION="Game_projects_group10\\assets\image\LEVEL3.png"
-BACKGROUND_START_LOCATION='Game_projects_group10\\assets\image\wall.png'
-SETTING_IMAGE_LOCATION='Game_projects_group10\\assets\image\setting.png'
-
-
 
 #################
 ### FOUCTION ####
